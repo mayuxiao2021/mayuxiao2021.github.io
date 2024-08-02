8c4d63ad222b00750f5b29b8935bfd5eb0bd8a4c
mayuxiao2021 <mayuxiao2021@outlook.com>
2024-08-02 13:59:02 +0800

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://github.glmyx.eu.org",
   base: "/",
   trailingSlash: "always",
   integrations: [
